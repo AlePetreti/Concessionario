a4ca914802194d4694f1a08e8e484782df28ac50
AlePetreti <petretiale@gmail.com>
2024-09-19 16:57:49 +0200

    sistemato errore package

---
+++ src/concessionario/model/leasing/Leasing.java
@@ -0,0 +1,44 @@
+package concessionario.model.leasing;
+
+import java.util.List;
+
+import concessionario.model.autonoleggio.auto_noleggio.AutomobileNoleggio;
+
+public class Leasing {
+
+    
+    private List<AutomobileNoleggio> automobili;
+
+    public List<AutomobileNoleggio> getAutomobili() {
+        return automobili;
+    }
+    private String autoSelezionata;
+    private int durataContratto;
+    private double prezzoMensile;
+    private int limiteChilometrico;
+
+    public Leasing(String autoSelezionata, int durataContratto, double prezzoMensile, int limiteChilometrico) {
+        this.autoSelezionata = autoSelezionata;
+        this.durataContratto = durataContratto;
+        this.prezzoMensile = prezzoMensile;
+        this.limiteChilometrico = limiteChilometrico;
+    }
+
+
+    public String getAutoSelezionata() {
+        return autoSelezionata;
+    }
+
+    public int getDurataContratto() {
+        return durataContratto;
+    }
+
+    public double getPrezzoMensile() {
+        return prezzoMensile;
+    }
+
+    public int getLimiteChilometrico() {
+        return limiteChilometrico;
+    }
+}
+
